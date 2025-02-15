02290be724a4da88727785e6877457ac061d64d9
Shane Barden <115955531+Shaneb7102@users.noreply.github.com>
2025-02-15 15:40:50 +0000

    Spring Initializr
    This is the first commit. This commit contains the spring initializr that will be used for the back end of my prototype

---
+++ sso-broker/sso-broker/src/main/java/com/example/sso_broker/SingleSignOnBrokerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sso_broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleSignOnBrokerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleSignOnBrokerBackendApplication.class, args);
+	}
+
+}
---
+++ sso-broker/sso-broker/src/test/java/com/example/sso_broker/SingleSignOnBrokerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sso_broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleSignOnBrokerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
