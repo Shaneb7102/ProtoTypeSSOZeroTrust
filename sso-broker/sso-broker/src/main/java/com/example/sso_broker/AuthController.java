cc392ea5a264999039ac0531d553e8fd397a8273
Shane Barden <shanebar@live.ie>
2025-02-16 19:06:28 +0000

    Small Modifications

---
+++ sso-broker/sso-broker/src/main/java/com/example/sso_broker/AuthController.java
@@ -0,0 +1,17 @@
+package com.example.sso_broker;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    @GetMapping("/user")
+    public OidcUser getUser(@AuthenticationPrincipal OidcUser user) {
+        return user;
+    }
+}
